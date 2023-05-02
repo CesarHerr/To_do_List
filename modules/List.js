e8b9b2ceb32d8cdec78332ed161d898aa92a76a4
Cesar Herrera <cesarherr321@gmail.com>
2023-05-02 15:22:46 -0400

    Fix linter errors for eslint and stylelint

--- modules/List.js
+++ modules/List.js
@@ -2,25 +2,25 @@ const list = [
   {
     index: 0,
     complete: true,
-    description: 'Do the dishes'
+    description: 'Do the dishes',
   },
   {
     index: 1,
     complete: false,
-    description: 'Study Maths'
+    description: 'Study Maths',
   },
   {
     index: 2,
     complete: true,
-    description: 'Do exercises'
-  }
+    description: 'Do exercises',
+  },
 ];
 
 const toDoList = () => {
-  let checkList = [];
+  const checkList = [];
 
   const main = document.querySelector('.todo__list');
-  list.forEach((parameter,index) => {
+  list.forEach((parameter, index) => {
     const toDo = document.createElement('li');
     toDo.innerHTML = `
       <span>
@@ -31,11 +31,10 @@ const toDoList = () => {
         
       <button class="button remove-btn" data-list-index=${index}><i class="fa-regular fa-trash-can" style="color: #7c889c;"></i></button> 
       `;
-      checkList.push(toDo)
+    checkList.push(toDo);
   });
 
   main.append(...checkList);
-
-}
+};
 
 export default toDoList;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import _, { forEach } from 'lodash';
 import './style.css';
-import toDoList from '../modules/List';
+import toDoList from '../modules/List.js';
 
 toDoList();
