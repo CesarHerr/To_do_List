32f4875e7b1615f601fc3d67bb08be9932e63f6f
Cesar Herrera <cesarherr321@gmail.com>
2023-05-03 20:17:23 -0400

    Add edit, remove method to the main class

--- modules/List.js
+++ modules/List.js
@@ -1,46 +1,53 @@
-const arrList = [
-  {
-    index: 0,
-    complete: true,
-    description: 'Do the dishes',
-  },
-];
-
 class Todo {
-  constructor(value) {
-    this.value = value;
+  constructor(text, complete = false) {
+    this.text = text;
+    this.complete = false;
   }
 }
 
-const mainList = document.querySelector('.todo__list');
+const mainList = document.querySelector('.tasks');
 
 class TodoList {
   constructor() {
-    this.list = [];
+    this.list = JSON.parse(localStorage.getItem('list')) || [];
+    this.displayList();    
+    this.enterEvent();
   }
 
-  add(value) {
-    const newTodo = new Todo(value);
+  add(text) {
+    const newTodo = new Todo(text);
     this.list.push(newTodo);
+    localStorage.setItem('list', JSON.stringify(this.list));
+    this.displayList();
   }
 
   remove(index) {
-    this.list.splice(index,1);    
+    this.list.splice(index, 1);
+    localStorage.setItem('list', JSON.stringify(this.list));
+    this.displayList();
+  }
+
+  edit(index, newText) {
+    this.list[index].text = newText;
+    localStorage.setItem('list', JSON.stringify(this.list));
+    this.displayList();
   }
 
   displayList() {
-    mainList.innerHTML = this.list.map(
-      (Todo, index) => `
-      <span>
-        <label for="name${list[index].index}"><label> 
-        <input name="name${list[index].index}" type="checkbox">
-        <p>${list[index].description}</p>             
-      </span>
-        
-      <button class="button remove-btn" data-list-index=${index}><i class="fa-regular fa-trash-can" style="color: #7c889c;"></i></button>      
-      `,
-    )
-    .join('');    
+    mainList.innerHTML = this.list
+      .map(
+        (todo, index) => `
+        <li>
+          <span>           
+            <input type="checkbox">
+            <label for="name${index}"></label>
+            <input name="name${index}" class="taskElement" value="${todo.text}"></input>             
+          </span>          
+          <button class="button remove-btn" data-list-index="${index}"><i class="fa-regular fa-trash-can" style="color: #a0a4ac;"></i></button>      
+        </li>
+      `
+      )
+      .join('');
   }
 
   addRemoveBtnListeners() {
@@ -48,9 +55,9 @@ class TodoList {
       if (event.target.classList.contains('remove-btn')) {
         const index = event.target.dataset.listIndex;
         this.remove(index);
-        this.displayList();
       }
     });
+    
   }
 }
 
